800a58986314d01b0e6b3a80ca81492c53bf94e5
10019610 <98252574+10019610@users.noreply.github.com>
2023-03-17 09:27:12 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    var answer = '';
+    for(let i = 0; i < my_string.length; i++){
+        
+    }
+        answer = my_string.toLowerCase()
+    let arr = answer.split("")
+  answer =  arr.sort()
+    
+    
+    return answer.join("");
+}
