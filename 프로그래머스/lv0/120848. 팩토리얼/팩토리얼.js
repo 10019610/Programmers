f0f27c52257540df3723430e9c95409d6f873ec9
10019610 <98252574+10019610@users.noreply.github.com>
2023-03-21 23:21:50 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    var answer = 1;
+    
+    for(let i = 1; i <=n ; i++){
+        answer *= i
+    if(answer === n){
+            return i;
+        }
+        
+        else if(answer > n){
+            return i - 1;
+        }
+    }
+        
+    return answer;
+}
