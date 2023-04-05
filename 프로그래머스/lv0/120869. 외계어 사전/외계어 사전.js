1b512e6441847094d662e987e9ddb3babbf48124
10019610 <98252574+10019610@users.noreply.github.com>
2023-04-05 19:29:31 +0900

    [level 0] Title: 외계어 사전, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,17 +1,25 @@
 function solution(spell, dic) {
     // spell의 글자가 모두 포함된 dic이 있다면 return 1; else 2
-    let spellArr = []
-    dic.map((word) => {
-        let count = 0;
-        spell.map((item)=>{
-            if(word.includes(item)){
+    let check = []
+    
+    // dic의 요소에 spell이 포함된 개수를 count함
+    dic.map((dicItem) => {
+        let count = 0;  // 겹치는 spell을 count하는 변수
+        spell.map((spellItem)=>{ // spell의 요소를 
+            if(dicItem.includes(spellItem)){
                 count++
             }
         })
-        if(count === spell.length){
-            spellArr.push(word)
+        if(count === spell.length){ // 겹치는 횟수가 spell의 개수와 같다 = spell의 요소로 구성됨 
+            check.push(dicItem)           // check array에 dic의 요소를 push
         }
     })
+    let answer = 0;                 // 
+    if(check.length === 0){
+        answer = 2
+    }else {
+        answer = 1
+    }
     
-    return spellArr.length === 0 ? 2 : 1;
+    return answer;
 }
