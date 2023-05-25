04b56b9a5e6cc3b9ff2bee883183d4f81433a541
10019610 <98252574+10019610@users.noreply.github.com>
2023-05-25 09:32:52 +0900

    [level 1] Title: 소수 찾기, Time: 95.19 ms, Memory: 44.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,10 @@
+function solution(n) {
+   const arr = new Array(n).fill(1)
+   arr[0] = 0;
+    for(let i = 2; i*i <= n; i++){
+        for(let j = i*i; j <= n; j+= i){
+            arr[j-1]=0
+        }
+    }
+    return arr.filter((i)=> i==1).length;
+}
